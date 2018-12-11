86552058ffc3a8ba451b0d13b31970555580e628
san <1298267898@qq.com>
2018-12-11 11:38:18 +0800

    随随便便 挂一个钩子

--- app/src/main/java/com/weisente/xpose/MainActivity.java
+++ app/src/main/java/com/weisente/xpose/MainActivity.java
@@ -1,13 +1,114 @@
 package com.weisente.xpose;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.ref.WeakReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Messenger mService;
+    private Messenger mReplyMessenger;
+    private Bitmap mDefaultIcon;
+    private Bundle data = new Bundle();
+    private Message msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button wx_button = (Button) findViewById(R.id.wx_bt);
+        Button qq_button = (Button) findViewById(R.id.qq_bt);
+
+        wx_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initQrData();
+                msg = Message.obtain(null,Constant.WE_GENERATE_QR);
+                msg.setData(data);
+                try {
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        qq_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initQrData();
+            }
+        });
+
+
+        Intent service = new Intent();
+        service.setClassName(this, "com.weisente.xpose.CommandService");
+        bindService(service, mConnection, BIND_AUTO_CREATE);
+
+        mDefaultIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.icon_head);
+        mReplyMessenger = new Messenger(new ClientMessengerHandler(this));
+
+    }
+
+    public void initQrData(){
+        data.putString(Constant.QR_DES,"test");
+        data.putFloat(Constant.QR_MONEY, 22.00f);
+        data.putParcelable(Constant.QR_ICON,mDefaultIcon);
+    }
+
+
+
+
+    private static class ClientMessengerHandler extends Handler{
+
+        private WeakReference<MainActivity> mActivity;
+
+        ClientMessengerHandler(MainActivity activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
+
+    }
+
+
+
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = new Messenger(service);
+            Message msg = Message.obtain(null, Constant.TEST_JOIN);
+            try {
+                msg.replyTo = mReplyMessenger;
+                mService.send(msg);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mConnection);
     }
 }
--- app/src/main/java/com/weisente/xpose/MainIntercept.java
+++ app/src/main/java/com/weisente/xpose/MainIntercept.java
@@ -1,9 +1,14 @@
 package com.weisente.xpose;
 
+import android.content.Context;
 import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import me.ele.uetool.UETool;
 
 public class MainIntercept implements IXposedHookLoadPackage {
 
@@ -11,8 +16,46 @@ public class MainIntercept implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if(lpparam.packageName.equals("com.tencent.mm")){
+        //确定是什么包发出的请求
+        if(lpparam.packageName.equals("com.tencent.mobileqq")){
             Log.d(TAG,"拦截 packageName: " + lpparam.packageName);
+//            hookcContext(lpparam.classLoader);
+//            hookstep(lpparam.classLoader);
         }
+    }
+    //修改qq的步数
+    private void hookstep(ClassLoader loader) {
+        try {
+            //4.4 nexus 通过
+            Class clazz = XposedHelpers.findClass("android.hardware.SystemSensorManager$SensorEventQueue", loader);
+            XposedBridge.hookAllMethods(clazz, "dispatchSensorEvent", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                    ((float[]) param.args[1])[0] = (((float[]) param.args[1])[0]) + 1234;
+                    Log.e(TAG,"asdasd"+((float[]) param.args[1])[0]);
+
+
+                }
+
+            });
+        } catch (Error | Exception e) {
+            e.printStackTrace();
+            Log.e(TAG,e.getMessage());
+        }
+
+
+    }
+
+
+    private void hookcContext(ClassLoader loader){
+        XposedHelpers.findAndHookMethod(Constant.QQ_app, loader, "onCreate",new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                Log.d(TAG,"拦截 UETool" );
+//                UETool.showUETMenu();
+            }
+        });
     }
 }
