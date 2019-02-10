ae1b6d4cf9105183f0a202f29369833b75c62fc1
san <1298267898@qq.com>
2019-02-10 14:32:51 +0800

    另一种方法实现免重启

--- app/src/main/java/com/weisente/xpose/MainActivity.java
+++ app/src/main/java/com/weisente/xpose/MainActivity.java
@@ -1,18 +1,8 @@
 package com.weisente.xpose;
 
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 
@@ -28,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Button wx_button = (Button) findViewById(R.id.wx_bt);
         Button qq_button = (Button) findViewById(R.id.qq_bt);
 
+        
 
 
 
